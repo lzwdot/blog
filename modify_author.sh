9b2d0578e31309a72b359cc192039a8af2e37c0b
lzw <bob.922@qq.com>
2021-08-09 12:17:03 +0800

    11

--- modify_author.sh
+++ modify_author.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 
 git filter-branch --env-filter '
-an="$GIT_AUTHOR_NAME"
-am="$GIT_AUTHOR_EMAIL"
-cn="$GIT_COMMITTER_NAME"
-cm="$GIT_COMMITTER_EMAIL"
 
-if [ "$GIT_COMMITTER_EMAIL" = "[bob.922@qq.com]" ]
+OLD_EMAIL="bob.922@qq.com"
+CORRECT_NAME="A.wei"
+CORRECT_EMAIL="lzw22@qq.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
-    cn="[A.wei]"
-    cm="[lzw22@qq.com]"
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
 
-if [ "$GIT_AUTHOR_EMAIL" = "[lzw22@qq.com]" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
-    an="[A.wei]"
-    am="[lzw22@qq.com]"
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
 
-export GIT_AUTHOR_NAME="$an"
-export GIT_AUTHOR_EMAIL="$am"
-export GIT_COMMITTER_NAME="$cn"
-export GIT_COMMITTER_EMAIL="$cm"
-'
+' --tag-name-filter cat -- --branches --tags
