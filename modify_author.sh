2f2040cdc925ad9ab9fe98f7d98efc6124bb3df7
lzw <bob.922@qq.com>
2021-08-09 11:26:39 +0800

    1

--- modify_author.sh
+++ modify_author.sh
@@ -1,3 +1,40 @@
 #!/bin/sh
 
-git filter-branch --env-filter ' OLD_EMAIL="lzw22@qq.com" CORRECT_NAME="A.wei" CORRECT_EMAIL="lzw22@qq.com" if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ] then export GIT_COMMITTER_NAME="$CORRECT_NAME" export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL" fi if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ] then export GIT_AUTHOR_NAME="$CORRECT_NAME" export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL" fi ' --tag-name-filter cat -- --branches --tags
+git filter-branch -f --env-filter '
+
+an="$GIT_AUTHOR_NAME"
+
+am="$GIT_AUTHOR_EMAIL"
+
+cn="$GIT_COMMITTER_NAME"
+
+cm="$GIT_COMMITTER_EMAIL"
+
+if [ "$GIT_COMMITTER_EMAIL" = "[lzw22@qq.com]" ]
+
+then
+
+    cn="[A.wei]"
+
+    cm="[lzw22@qq.com]"
+
+fi
+
+if [ "$GIT_AUTHOR_EMAIL" = "[lzw22@qq.com]" ]
+
+then
+
+    an="[A.wei]"
+
+    am="[lzw22@qq.com]"
+
+fi
+
+export GIT_AUTHOR_NAME="$an"
+
+export GIT_AUTHOR_EMAIL="$am"
+
+export GIT_COMMITTER_NAME="$cn"
+
+export GIT_COMMITTER_EMAIL="$cm"
+'
