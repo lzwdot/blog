87daa899465bfc18da36402481e549142915c5e3
lzw <bob.922@qq.com>
2021-08-20 23:49:44 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
 git remote set-url origin https://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@github.com/awei922/blogs.git
-
-git remote set-url origin https://<your_token>@github.com/<USERNAME>/<REPO>.git
