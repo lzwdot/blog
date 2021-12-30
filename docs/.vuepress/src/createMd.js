4fd3f72656d6afcb2fc19f5233619e546a0d0da4
lzw <lzwdot@qq.com>
2021-12-30 21:36:21 +0800

    update project

--- docs/.vuepress/src/createMd.js
+++ docs/.vuepress/src/createMd.js
@@ -9,7 +9,7 @@ const docPath = path.join(__dirname, '../../') // 获取文档目录
 
 /*******新建文章 -start*******/
 
-const dirPath = `engineering/webpack-study`
+const dirPath = `product/project`
 const title = '标题'
 
 // getMaxId()
