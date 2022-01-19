17fba18b036d305908c4a8d4f6d8628f8bef33e3
lzw <lzwdot@qq.com>
2022-01-19 17:37:41 +0800

    update

--- docs/.vuepress/src/createMd.js
+++ docs/.vuepress/src/createMd.js
@@ -9,7 +9,7 @@ const docPath = path.join(__dirname, '../../') // 获取文档目录
 
 /*******新建文章 -start*******/
 
-const dirPath = `async/js-asyn`
+const dirPath = `javascript/js-faq`
 const title = '标题'
 
 // getMaxId()
