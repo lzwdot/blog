4147b6f1445caf9ef9c24fc0078f0837fcc21367
lzw <lzwdot@qq.com>
2022-01-20 16:05:48 +0800

    toref

--- docs/.vuepress/src/createMd.js
+++ docs/.vuepress/src/createMd.js
@@ -9,7 +9,7 @@ const docPath = path.join(__dirname, '../../') // 获取文档目录
 
 /*******新建文章 -start*******/
 
-const dirPath = `javascript/js-faq`
+const dirPath = `frame/vuejs3-base`
 const title = '标题'
 
 // getMaxId()
