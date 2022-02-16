e03d03bb9a0e22cdc648613ead9246b8eb8ac053
lzw <lzwdot@qq.com>
2022-02-16 21:05:47 +0800

    update

--- docs/.vuepress/clientAppEnhance.js
+++ docs/.vuepress/clientAppEnhance.js
@@ -2,9 +2,18 @@ import { defineClientAppEnhance } from '@vuepress/client'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 
+// 监控
+import * as fundebug from "fundebug-javascript";
+import fundebugVue from "fundebug-vue";
+fundebug.apikey = "7f066c99228ca076b3eaed18791d7966a36e205f61c9762c7f803015a9454ace"
+
+
 export default defineClientAppEnhance(({ app, router, siteData }) => {
     // 使用 Element UI Vuejs 3.x 版本
-    app.use(ElementPlus)    
+    app.use(ElementPlus) 
+    
+    // 监控
+    fundebugVue(fundebug, app);
 
     // 路由切换事件处理
     router.beforeEach((to, from, next) => {
